0751899967ba4a0f9b8d5af78eb258d2706d0eb2
barraud-samuel <samuel.barraud@hetic.net>
2020-01-11 21:33:57 +0100

    add teams to api

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const db = require('./db');
+const team = require('./team');
 
 app.get('/', (req, res) => res.send(db))
+app.get('/teams', (req, res) => res.send(team))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
