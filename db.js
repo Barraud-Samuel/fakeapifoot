87c4b53d3078764df5560497bed345b982ff0da4
barraud-samuel <samuel.barraud@hetic.net>
2020-02-04 00:13:14 +0100

    update match

--- db.js
+++ db.js
@@ -31,7 +31,7 @@ module.exports  ={
                     "team_name": "HIFK Elsinki",
                     "logo": "https://media.api-football.com/teams/1172.png"
                 },
-                "goalsHomeTeam": 1,
+                "goalsHomeTeam": 2,
                 "goalsAwayTeam": 1,
                 "score": {
                     "halftime": null,
